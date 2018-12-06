40bfa7700a0ff7c326ac159ee10764e6fd018738
Przemyslaw Przewoznik <blagoff88@gmail.com>
2018-12-07 00:53:19 +0100

    07.12.18

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var elem = document.querySelector('.main-carousel');
+
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  imagesLoaded: true,
+  lazyload: true,
+  hash: true
+}
+);
+
+/*var flkty2 = new Flickity('.carousel');
+
+var previousWrappedButton = document.querySelector('.button--previous-wrapped');
+previousWrappedButton.addEventListener( 'click', function() {
+  flkty2.previous( true );
+ 
+});*/
+
+
+
+
+
+
+
+
+
+
+
+
